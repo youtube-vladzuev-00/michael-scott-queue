8383548b09a944e9f0ea1b4d292c1bb76b0dfc12
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2024-09-08 18:47:18 +0300

    enqueue #3

--- ConcurrentQueue.java
+++ ConcurrentQueue.java
@@ -1,4 +1,5 @@
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 public final class ConcurrentQueue<E> {
     private volatile Node<E> head;
@@ -13,8 +14,7 @@ public final class ConcurrentQueue<E> {
     public void enqueue(final E element) {
         final Node<E> newNode = new Node<>(element);
         while (true) {
-            if (tail.next == null) {
-                tail.next = newNode;
+            if (tail.next.compareAndSet(null, newNode)) {
                 break;
             }
         }
@@ -27,14 +27,15 @@ public final class ConcurrentQueue<E> {
 
     private static final class Node<E> {
         private volatile E value;
-        private volatile Node<E> next;
+        private final AtomicReference<Node<E>> next;
 
         public Node() {
-
+            next = new AtomicReference<>();
         }
 
         public Node(final E value) {
             this.value = value;
+            next = new AtomicReference<>();
         }
     }
 }
